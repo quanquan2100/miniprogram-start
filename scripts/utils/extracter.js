b001593dde7a9c7deb377ae6088d3c68e9d6c90d
Zhanglinling <zhanglinling0926@gmail.com>
2018-06-01 17:57:49 +0800

    改进提示

--- scripts/utils/extracter.js
+++ scripts/utils/extracter.js
@@ -8,7 +8,7 @@ const task = process.env.NODE_ENV === 'production' ? 'build' : 'dev';
 
 require('shelljs/global');
 
-console.log("extracter", process.env.NODE_ENV)
+console.log(isProduction ? "开始构建项目 >>>>>>>" : "进入开发模式 >>>>>>>")
 
 module.exports = function(config = {}) {
   // 清空 dist 目录
@@ -29,7 +29,7 @@ function extracter(config = {}) {
       const extname = path.extname(src);
 
       if (isProduction) {
-        // 如果是构建模式, 需处理 js, pcss, img
+        // 如果是构建模式, 处理所有资源
         return ['.js', '.pcss', '.json', '.wxml', '.md', '.jpg', '.jpeg', '.png', '.gif', '.svg'].indexOf(extname) < 0;
       } else {
         // 如果是开发模式, 仅处理 js, pcss 即可
@@ -40,15 +40,18 @@ function extracter(config = {}) {
 
   // 编译 js wxss img 文件
   exec(`gulp ${task} --gulpfile scripts/utils/build.js --dist ${config.dist} --src ${config.src} --color`);
-  console.log("执行完毕, 打开小程序")
-  // http://127.0.0.1:22295/open?projectpath=%E9%A1%B9%E7%9B%AE%E5%85%A8%E8%B7%AF%E5%BE%84
-  const req = http.request({
-    port: 22295,
-    path: '/open?projectpath=' + encodeURIComponent('/Users/kuaikan/Documents/GitHub/miniprogram-begintemplate/dist')
-  })
 
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
+  if (isProduction) {
+    console.log("构建完毕, 尝试打开小程序")
+    const req = http.request({
+      port: 22295,
+      path: '/open?projectpath=' + encodeURIComponent('/Users/kuaikan/Documents/GitHub/miniprogram-begintemplate/dist')
+    })
 
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+    });
+  } else {
+    console.log("编译成功, 监听中...")
+  }
 }
