89ba95cfb33bf2f6f1744143e21290fb7a815ea4
Zhanglinling <zhanglinling0926@gmail.com>
2018-06-03 22:14:56 +0800

    添加构建完成自动打开小程序

--- scripts/utils/extracter.js
+++ scripts/utils/extracter.js
@@ -4,6 +4,7 @@ const nodeWatch = require('node-watch');
 const http = require('http');
 require('shelljs/global');
 const chalk = require('chalk');
+const os = require('os');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const regx = /project\.config\.json$/; // 项目配置文件正则
@@ -45,27 +46,20 @@ module.exports = function(config = {}) {
 
   // 2. 监听文件变动
   if (isProduction) {
-    console.log("构建完毕, 尝试打开小程序")
-    const req = http.request({
-      port: 22295,
-      path: '/open?projectpath=' + encodeURIComponent('/Users/kuaikan/Documents/GitHub/miniprogram-begintemplate/dist')
-    })
-
-    req.on('error', (e) => {
-      console.error(`打开失败 ${e.message}`);
-    });
+    console.log("构建完毕, 尝试打开小程序");
+    openWxIDE(config);
   } else {
     nodeWatch(config.src, {
-        recursive: true,
-        filter: (src) => {
-          return (!regx.test(src) && !regx_md.test(src))
-        }
+      recursive: true,
+      filter: (src) => {
+        return (!regx.test(src) && !regx_md.test(src))
+      }
     }, (evt, name) => {
       if (evt == 'update') {
         // on create or modify, run compile
         const extname = path.extname(name);
         const distName = getDistPath(name, config);
-        switch(true) {
+        switch (true) {
           case ['.js'].indexOf(extname) >= 0:
             exec(`gulp compile-js --gulpfile scripts/utils/gulp-tasks/compile-js.js --dist ${distName} --src ${name} --color`);
             break;
@@ -74,11 +68,11 @@ module.exports = function(config = {}) {
             break;
           default:
             // 执行拷贝
-            console.log('copy',  chalk.blue.bold(name) , 'to' , chalk.blue.bold(distName))
+            console.log('copy', chalk.blue.bold(name), 'to', chalk.blue.bold(distName))
             fs.copySync(name, distName);
         }
       }
-     
+
       if (evt == 'remove') {
         // on delete, run del
         const distName = getDistPath(name, config);
@@ -97,5 +91,36 @@ module.exports = function(config = {}) {
 
 
 function getDistPath(src, config) {
-  return src.replace(config.src,config.dist);
+  return src.replace(config.src, config.dist);
+}
+
+function openWxIDE(config) {
+  let portFile = path.join(os.homedir(), 'Library/Application\ Support/微信web开发者工具/Default/.ide');
+  if (process.platform === 'win32') {
+    portFile = path.join(os.homedir(), 'AppData/Local/微信web开发者工具/User\ Data/Default/.ide');
+  }
+  try {
+    const port = fs.readFileSync(portFile).toString();
+    exec(`curl 127.0.0.1:${port}/open?projectpath=${encodeURI(config.dist)} -s`)
+    // let opt = {
+    //   protocol:'http:',
+    //   hostname:'127.0.0.1',
+    //   port:port,
+    //   path:'/open?projectpath='+config.dist
+    // };
+    // console.log(opt);
+    // http.request(opt,function(res){
+    //   res.setEncoding('utf8');
+    //   res.on('data', (chunk) => {
+    //     console.log(`BODY: ${chunk}`);
+    //   });
+    //   res.on('end', () => {
+    //     console.log('No more data in response.');
+    //   });
+    // });
+
+  } catch (error) {
+    console.log("微信IDE未打开", error);
+  }
+
 }
