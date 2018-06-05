05c218afe4bccb53b5753d937d900dfc2ca53c1c
Zhanglinling <zhanglinling0926@gmail.com>
2018-06-05 10:52:41 +0800

    添加小程序页面间事件系统

--- src/utils/event.js
+++ src/utils/event.js
@@ -1,3 +1,170 @@
 /**
- * 补充小程序缺失的事件系统
+ * 补充小程序缺失的页面间事件系统
  */
+
+exports.$runEvent = function(event) {
+
+  // 使用闭包以支持异步函数
+  this.$eventPool.forEach(listen => {
+    if (listen.name !== event.name) {
+      return;
+    }
+    if (listen.forceNow) {
+      (function(_this, listen, event) {
+        console.log(arguments)
+        listen.callback(event)
+
+        if (listen.once) {
+          listen = null;
+        }
+      })(this, listen, event);
+    } else {
+      (function(_this, listen, event) {
+        _this.$eventCatch = _this.$eventCatch || [];
+        _this.$eventCatch.push({
+          callback: listen.callback,
+          params: event
+        })
+
+        if (listen.once) {
+          listen = null;
+        }
+      })(this, listen, event);
+    }
+  })
+};
+
+exports.$runCatchEvent = function () {
+  if (!Array.isArray(this.$eventCatch)) {
+    return;
+  }
+  this.$eventCatch.forEach(item => {
+    (function (item) {
+      item.callback(item.params)
+    }(item))
+  })
+}
+
+/**
+ * 监听事件功能
+ * @param  {function} callback 监听到事件的回调函数
+ * @param  {boolean} now 是否立即反应
+ * @return {string}          监听id
+ */
+exports.$on = function(evtName = "", callback = null, now = false) {
+  if (!evtName || typeof callback !== "function") {
+    return;
+  }
+
+  // step:1 创建监听对象
+  this.$eventPool = this.$eventPool || [];
+  const key = Symbol(evtName);
+  const listen = {
+    id: key,
+    name: evtName,
+    callback: callback.bind(this),
+    forceNow: now,
+    once: false
+  };
+
+  // step:2 对象加入事件池
+  this.$eventPool.push(listen)
+
+  // step:3 返回监听id
+  return key;
+};
+
+/**
+ * 仅监听一次功能
+ * @param  {function} callback 监听到事件的回调函数
+ * @param  {boolean} now 是否立即反应, 否则当页面 onshow 的时候反应
+ * @return {string}          监听id
+ */
+exports.$one = function(evtName = "", callback = null, now = false) {
+  if (!evtName || typeof callback !== "function") {
+    return;
+  }
+
+  // step:1 创建监听对象
+  this.$eventPool = this.$eventPool || [];
+  const key = Symbol(evtName);
+  const listen = {
+    id: key,
+    name: evtName,
+    callback: callback.bind(this),
+    forceNow: now,
+    once: true
+  };
+
+  // step:2 对象加入事件池
+  this.$eventPool.push(listen)
+
+  // step:3 返回监听id
+  return key;
+};
+
+/**
+ * 取消监听
+ * @param  {[type]} argument [description]
+ * @return {[type]}          [description]
+ */
+exports.$off = function(listenId = "") {
+  // step:1 参数验证
+  if (!listenId) {
+    return;
+  }
+
+  this.$eventPool = this.$eventPool || [];
+  this.$eventPool.filter(item => item.id !== listenId)
+};
+
+/**
+ * 触发事件功能
+ * @param  {[type]} argument [description]
+ * @return {[type]}          [description]
+ */
+exports.$emit = function(name = "", data = null) {
+  // step:1 构建事件对象
+  const event = _createEvent(name, data, this);
+
+  // step:2 事件广播
+  setTimeout(() => {
+    _eventBus(event);
+  })
+};
+
+/**
+ * 创建事件对象
+ * @param  {String} name  [description]
+ * @param  {[type]} data  [description]
+ * @param  {[type]} _this [description]
+ * @return {[type]}       [description]
+ */
+function _createEvent(name = "", data = null, _this = null) {
+  // 参数验证
+  if (name === "") {
+    throw "Event name is required";
+  }
+  // TODO: 判断 _this 为 page
+  const event = {};
+  event.createTime = Date.now();
+  event.name = name;
+  event.source = _this;
+  event.data = data;
+  return event;
+}
+
+/**
+ * 事件传递
+ * @param  {[type]} event [description]
+ * @return {[type]}       [description]
+ */
+function _eventBus(event) {
+  const pages = getCurrentPages();
+
+  // 倒序遍历页面栈
+  for (let i = pages.length - 1; i >= 0; i--) {
+    // 尝试执行每个页面的监听
+    pages[i].$runEvent(event)
+  }
+}
--- template/page/index.js
+++ template/page/index.js
@@ -1,66 +1,37 @@
-// pages/test.js
-Page({
+const Event = require('../../utils/event.js'); // TODO: 微信小程序无绝对路径问题
+const app = getApp();
 
+Page({
+  ...Event,
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.$runCatchEvent();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
+  onUnload: function() {},
 })
